38d5f9a2d8a8baf5f10b9dc6d7bc78f565f43618
Andrey Pestunov <masterlifting@gmail.com>
2023-03-03 16:55:11 +0100

    feat: recover code base

---
+++ src/Net.Shared.Web/Exceptions/NetSharedWebException.cs
@@ -0,0 +1,14 @@
+﻿using Net.Shared.Exceptions;
+
+namespace Net.Shared.Web.Exceptions;
+
+public sealed class NetSharedWebException : NetSharedException
+{
+    public NetSharedWebException(string message) : base(message)
+    {
+    }
+
+    public NetSharedWebException(Exception exception) : base(exception)
+    {
+    }
+}
--- src/Net.Shared.Web/Exceptions/SharedWebException.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shared.Exceptions.Abstractions;
-using Shared.Exceptions.Models;
-
-namespace Net.Shared.Web.Exceptions;
-
-public sealed class SharedWebException : SharedException
-{
-    public SharedWebException(string initiator, string action, ExceptionDescription description) : base(initiator, action, description) { }
-}
